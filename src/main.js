3a8f761214cc98abb53ec56a0071e3294e3beed5
xingHome <xingqq2233@qq.com>
2021-11-02 14:37:01 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/main.less'
+import axios from 'axios'
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App)
